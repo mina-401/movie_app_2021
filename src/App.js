35ebfe7beb855e6838f625b8f761e35017ccf3fe
mina-401 <mina-401@gmail.com>
2021-07-29 03:03:35 +0900

    Create css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import Red from 'Red';
-function App() {
-  return (
-    <div>hello~
-      <Red />
-    </div>
-  );
+import axios from  "axios";
+import Movie from "./Movie";
+import "./App.css";
+
+class App extends React.Component {
+  state={
+    isLoading:true,
+    movies:[]
+  };
+  getMovies=async()=>{
+    const {data:{data:{movies}}}=await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies,isLoading:false});
+  };
+  componentDidMount(){
+    this.getMovies();
+  }
+  render(){
+    const {isLoading,movies}=this.state;
+
+    return (
+      <section class="container">
+        {isLoading?(
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ):(
+          <div class="movies">
+            {movies.map(movie=>(
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+      
+    );
+  }
 }
+
 
 export default App;
