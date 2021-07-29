2f71a4ada0d67c96dc31373ec72a587a770228f1
mina-401 <mina-401@gmail.com>
2021-07-30 00:40:46 +0900

    Create components folder

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,8 +4,13 @@ import {Link} from "react-router-dom"
 function Navigation(){
     return
         <div>
+<<<<<<< Updated upstream
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
+=======
+            <Link href="/">Home</a>
+            <linkSync href="/about">About</a>
+>>>>>>> Stashed changes
         </div>
 }
 export default Navigation
