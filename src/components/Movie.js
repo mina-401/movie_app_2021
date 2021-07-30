489c31897006955e91d0003fbbbca2ba2a2e55ad
mina-401 <mina-401@gmail.com>
2021-07-30 12:11:59 +0900

    Add Link

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import "./Movie.css";
 
 function Movie({id,year,title,summary,poster,genres}){
     return (
-        <div className="movie">
-            <img src={poster} alt={title} title={title}/>
-            <div className="movie-data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <p className="movie__summary">{summary.slice(0,140)}</p>
-                <ul className="genres">
-                    {genres.map((genre,id)=>(
-                        <li key={id} className="genres__genre">{genre}</li>   
-                    ))}
-                </ul>
+        <Link to={{
+            pathname:`/movie/${id}`,
+            state:{
+                year,
+                title,
+                summary,
+                poster,
+                genres
+            }
+        }}>
+            <div className="movie">
+                <img src={poster} alt={title} title={title}/>
+                <div className="movie-data">
+                    <h3 className="movie__title">{title}</h3>
+                    <h5 className="movie__year">{year}</h5>
+                    <p className="movie__summary">{summary.slice(0,140)}</p>
+                    <ul className="genres">
+                        {genres.map((genre,id)=>(
+                            <li key={id} className="genres__genre">{genre}</li>   
+                        ))}
+                    </ul>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 Movie.propTypes={
